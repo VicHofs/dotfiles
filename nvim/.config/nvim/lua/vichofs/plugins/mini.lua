f74f42138d03d9de9d2cfea45ed5b2f377efb550
Victor Hofstetter <victorbarroshof@gmail.com>
2026-07-03 02:05:50 -0700

    :recycle: refactor(nvim): Restore surround defaults

--- nvim/.config/nvim/lua/vichofs/plugins/mini.lua
+++ nvim/.config/nvim/lua/vichofs/plugins/mini.lua
@@ -58,11 +58,11 @@ return {
             -- saw surround with whitespace
             mappings = {
                 add = 'sa',            -- Add surrounding in Normal and Visual modes
-                delete = 'ds',         -- Delete surrounding
+                delete = 'sd',         -- Delete surrounding
                 find = 'sf',           -- Find surrounding (to the right)
                 find_left = 'sF',      -- Find surrounding (to the left)
                 highlight = 'sh',      -- Highlight surrounding
-                replace = 'ca',        -- Replace surrounding
+                replace = 'sr',        -- Replace surrounding
                 update_n_lines = 'sn', -- Update `n_lines`
 
                 suffix_last = 'l',     -- Suffix to search with "prev" method
